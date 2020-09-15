748fd78af0f30d588c003681d7c219f24f83bf43
Eyub Yildirim <eyub2001@gmail.com>
2020-09-15 11:16:47 +0300

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,24 +3,29 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Verilen sayinin faktoriyeli: " + faktoriyel(5));
-        System.out.println("Faktoriyelin rakamlarinin toplami: " + rakamlarinToplami(faktoriyel(5)));
+        System.out.println("Faktoriyel: " + faktoriyel(6));
+        System.out.println("Rakamlarin toplami: " + rakamlarinToplami(faktoriyel(6)));
     }
 
     // Verilen sayinin faktoriyelini alir
     public static int faktoriyel(int sayi) {
+        // recursion
+
         if (sayi == 1)
             return sayi;
         else
             return (faktoriyel(sayi - 1)) * sayi;
     }
 
-    // Verilen sayinin rakamlarini toplar
+    // Verilen sayinin rakamlarinin toplamini alir
     public static int rakamlarinToplami(int sayi) {
+        // int sayi = 324 / 10 ===> 32
+        // 3249 % 10 = 9
+        // 3 / 10 = 0
+
         int toplam = 0;
 
-        // 0
-        while (sayi != 0) {
+        while (sayi != 0){
             toplam = toplam + (sayi % 10);
             sayi = sayi / 10;
         }
